d8a18ce8a3b7cee89cf740ffe6b954db3c4198b4
Ow3nV <c1004642@newcastle.ac.uk>
2023-06-13 02:41:06 +0100

    profile + logout

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -5,4 +5,6 @@ from authenticate import views
 urlpatterns = [
     path('register', views.register_request, name='register'),
     path('login', views.login_user, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('profile', views.profile_view, name='profile'),
 ]
