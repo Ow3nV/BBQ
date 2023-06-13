470b0228c2c5d1ec4b49660d1cecf8fac574c747
Ow3nV <c1004642@newcastle.ac.uk>
2023-06-13 03:29:07 +0100

    begun adminsite

--- bbqweb/urls.py
+++ bbqweb/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('home', views.home, name='home'),
+    path('rent', views.bbq_rent, name='rent_a_bbq'),
+    path('', views.index, name=''),
+    path('site_admin', views.admin, name='admin'),
 ]
--- bbqweb/views.py
+++ bbqweb/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
-
 def index(request):
+    return redirect('home')
+def home(request):
     return render(request, 'bbq/bbqhome.html')
 
 def bbq_rent(request):
     return render(request, 'bbq/bbqrent.html')
+
+
+def admin(request):
+    if request.user.is_superuser:
+        return render(request, 'admin/admin.html')
+    else:
+        return redirect('home')
