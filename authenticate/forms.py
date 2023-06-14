8e1b7d5954f33e4c780cac23877c1136a9f58a79
Ow3nV <c1004642@newcastle.ac.uk>
2023-06-14 15:57:02 +0100

    testing update

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
-    username = forms.CharField(blank=False, null=False, error_messages={'blank': 'INVALID!!11', 'null': 'NULL11!'})
+    username = forms.CharField(error_messages={'blank': 'INVALID!!11', 'null': 'NULL11!'})
     email = forms.EmailField(required=True)
     class Meta:
         model = User
