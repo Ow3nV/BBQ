be661c62f8d3f0742388f75c3b0ec7899d3f2682
Ow3nV <c1004642@newcastle.ac.uk>
2023-06-14 19:55:27 +0100

    small updates

--- order/models.py
+++ order/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from bbqweb.models import Barbeque
@@ -5,4 +6,16 @@ from bbqweb.models import Barbeque
 
 # Create your models here.
 class Order(models.Model):
-    user = models.ForeignKey(barbeque = models.ForeignKey(Barbeque, on_delete=models.CASCADE))
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    barbeque = models.ForeignKey(Barbeque, on_delete=models.CASCADE)
+    date_from = models.DateField()
+    date_to = models.DateField()
+    sub_total = models.IntegerField()
+    address_line_1 = models.CharField()
+    address_line_2 = models.CharField()
+    town_city = models.CharField()
+    county = models.CharField()
+    postcode = models.CharField()
+    delivery = models.BooleanField()
+    completed = models.BooleanField()
+
