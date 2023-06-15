7dd1c9c66721c9da3d3db720a944ce9a63e2855d
Ethan <ethan2003@gmail.com>
2023-06-15 14:56:55 +0100

    did some shit css

---
+++ static/brain.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Add an event listener to the support bubble
+  document.querySelector('.support-bubble').addEventListener('click', function() {
+  // Toggle the visibility of the popup
+  var popup = document.querySelector('.popup');
+  popup.style.display = popup.style.display === 'none' ? 'block' : 'none';
+});
+});
