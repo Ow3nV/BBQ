79e3bf56a83ee2e628f2f3a65b175c0e353c835c
Ethan <ethan2003@gmail.com>
2023-06-16 15:18:41 +0100

    we did maths in code

--- static/brain.js
+++ static/brain.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Add an event listener to the support bubble
-  document.querySelector('.support-bubble').addEventListener('click', function() {
+    document.querySelector('.support-bubble').addEventListener('click', function() {
   // Toggle the visibility of the popup
-  var popup = document.querySelector('.popup');
-  popup.style.display = popup.style.display === 'none' ? 'block' : 'none';
-});
+        var popup = document.querySelector('.popup');
+        popup.style.display = popup.style.display === 'none' ? 'block' : 'none';
+    });
+
 });
