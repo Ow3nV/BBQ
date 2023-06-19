b0ff4e2843a14f5bc9ffec2f599aba77a06e9faa
Ow3nV <c1004642@newcastle.ac.uk>
2023-06-19 18:03:10 +0100

    back to the start

--- BBQ/settings.py
+++ BBQ/settings.py
@@ -46,10 +46,6 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'order',
     'mathfilters',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -145,31 +141,9 @@ USE_L10N = False
 
 DATE_FORMAT = "%D/%M/%Y"
 
-SITE_ID = 2
-
-LOGIN_REDIRECT_URL = '/home'
-LOGOUT_REDIRECT_URL = '/home'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend'
-]
-
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
-        }
-    }
-}
 
