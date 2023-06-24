79aa918d1037902e3fe15747a7bc86bb31711ade
Ow3nV <c1004642@newcastle.ac.uk>
2023-06-24 17:49:32 +0100

    misc

--- authenticate/models.py
+++ authenticate/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class UserAddress(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address_line_1 = models.CharField(max_length=50)
+    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
     address_line_2 = models.CharField(max_length=50, blank=True, null=True)
-    town_city = models.CharField(max_length=50)
-    county_state = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
-    postcode = models.CharField(max_length=50)
+    town_city = models.CharField(max_length=50, blank=True, null=True)
+    county_state = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    postcode = models.CharField(max_length=50, blank=True, null=True)
